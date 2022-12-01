14aa76114400eb1386b89a521bdc6468c18187d6
Shawn <shawnaugustine01@gmail.com>
2022-12-01 16:37:13 +0000

    finsihed extension

--- safari_queries.sql
+++ safari_queries.sql
@@ -23,7 +23,37 @@ ON assignments.employee_id = staff.id
 WHERE enclosures.closedForMaintence = true;
 
 --name of enclosure where oldest animal lives. if 2 animals are same age, choose alphabetically.
-SELECT enclosures.name AS enclosure_name, MAX(animals.age) AS animal_age FROM enclosures
+SELECT MAX(animals.age) AS oldest_animals, enclosures.name AS enclosure_name FROM enclosures
 INNER JOIN animals 
 ON enclosures.id = animals.enclosure_id
-GROUP BY enclosure_name;
+GROUP BY enclosure_name
+ORDER BY oldest_animals DESC LIMIT 1;
+
+--number of different animal types a given keeper has been assigned to work with.
+SELECT COUNT(DISTINCT animals.type) FROM animals
+INNER JOIN enclosures
+ON enclosures.id = animals.enclosure_id
+INNER JOIN assignments 
+ON enclosures.id = assignments.enclosure_id
+INNER JOIN staff 
+ON assignments.employee_id = staff.id
+WHERE staff.name = 'Shiro';
+
+--number of different keepers who have been assigned to work in a given enclosure
+SELECT COUNT(DISTINCT staff.name) FROM enclosures
+INNER JOIN assignments 
+ON enclosures.id = assignments.enclosure_id
+INNER JOIN staff 
+ON assignments.employee_id = staff.id
+WHERE enclosures.name = 'BOB';
+
+--names of other animals sharing an enclosure with a given animal.
+SELECT animals.name FROM enclosures
+INNER JOIN animals 
+ON enclosures.id = animals.enclosure_id
+WHERE enclosures.name = (SELECT enclosures.name FROM enclosures
+INNER JOIN animals 
+ON enclosures.id = animals.enclosure_id
+WHERE animals.name = 'Batman') AND animals.name != 'Batman';
+
+
