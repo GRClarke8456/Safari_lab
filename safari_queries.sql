20ee5d8a8ab993170219cd0ef8a24c1a9861d626
Shawn <shawnaugustine01@gmail.com>
2022-12-01 15:14:30 +0000

    mvp queries done

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,5 +1,13 @@
-
-SELECT animals.name AS name FROM enclosures
+--name of animals in a given enclosure
+SELECT animals.name AS animal_name FROM enclosures
 INNER JOIN animals 
 ON enclosures.id = animals.enclosure_id
+WHERE enclosure.name = 'BOB';
+
+--name of staff working in a given enclosure
+SELECT staff.name AS staff_name FROM enclosures
+INNER JOIN assignments 
+ON enclosures.id = assignment.enclosure_id
+INNER JOIN staff 
+ON assignment.employee_id = staff.id
 WHERE enclosure.name = 'BOB';
